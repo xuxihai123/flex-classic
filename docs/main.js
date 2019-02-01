b521b7a67591a99e1207ecb13849cc71e7c908dc
xuxihai <xuxihai@360.net>
2019-02-01 22:32:24 +0800

    init

---
+++ docs/main.js
@@ -0,0 +1,15 @@
+loadjs([
+  'https://cdn.bootcss.com/jquery/1.11.3/jquery.min.js',
+  '/assets/css/prettify.css',
+  '/assets/js/prettify.min.js',
+], function() {
+  $(document).ready(function() {
+    var htmlCode = $('#html-source')[0].outerHTML;
+    var cssCode = $('#css-source')[0].innerText;
+    htmlCode = $('<pre class="prettyprint linenums" lang="html">' + htmlCode.replace(/</g, '&lt;') + '</pre>');
+    cssCode = $('<pre class="prettyprint linenums" lang="css">' + cssCode.replace(/\}\s+\n/g, '}\n').replace(/\n\s{8}/g,'\n') + '</pre>');
+    $('body').prepend(cssCode);
+    $('body').prepend(htmlCode);
+    prettyPrint();
+  });
+});
